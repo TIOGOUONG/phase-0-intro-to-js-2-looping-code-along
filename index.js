e945e33cce7930afc4fec6b0f8ef9a8a5eab663c
TIOGOUNG <+>
2022-04-08 19:27:09 -0400

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(array,events) {
+const  Sam =[]
+for (let i of array) {
+    Sam.push(`Thank you, ${i}, for the wonderful ${events} gift!`) 
+}
+return Sam
+}
+function countDown() {
+    let countDown =10;
+    while (countDown >= 0) {
+        console.log(countDown--) ;
+    }
+}
